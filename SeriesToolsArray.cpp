b29492b71719d939b6cc35c7c91000be1c927bb6
Blane <80710483+CSUltiGod99@users.noreply.github.com>
2024-10-21 19:53:27 +0100

    Update SeriesToolsArray.cpp

--- SeriesToolsArray.cpp
+++ SeriesToolsArray.cpp
@@ -1,19 +1,32 @@
 /*
   Create a Series of Tools used for
-  Integrated Software & how
+  Integrated Software & how to use them.
 *\
 
-#include <Vector>
+#include <vectors>
 #include <iostream>
-#include <element.h>
+#include <ShowingElements.h>
 
+class Tools
+{
+    int Too
 
+  
 
+  
+    int Semi_Colon()
+    {
+      std::Title = "D";
+      std::string semi_c = ";";
 
+      cout << Title << semi_c;
+    }
 
-
-
-
+    int Radiobutton()
+    {
+      Pic = 🔘;
+      int Radi = "
+    }
 
 
 
