747af3a0584fe18e74ddf7d5c0ad242e4c3acd6e
Blane <80710483+CSUltiGod99@users.noreply.github.com>
2024-10-24 19:02:56 +0100

    Update iterator_speed.cpp

--- iterator_speed.cpp
+++ iterator_speed.cpp
@@ -1,14 +1,21 @@
-/*Iteration Script for changing speed*/
+/*Iteration Script for changing speed:*/
 
 #include <iostream>
-#include <iteration
+#include <iteration>
+#include <vector>
 
 using namespace std;
 
 int iterator_m(v)
 {
+   int array_c[8] = { 12, 14, 22, 15, 34, 54, 119, 91};
+   vector<array_c>::iterator itArray;
   
-  
-  
+     for (it = array_c.begin(); it != array_c.end() ++it)
+     {
+        cout << *it << "\n";
+     }
 }
+
+
 
